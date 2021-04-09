ba92ddcae2df34870ddc400323057449741a48ab
thomca <catherine.thomsen@wallawalla.edu>
2021-04-08 21:41:17 -0700

    pizza guis and themes

--- Pizza_Party/app/src/main/java/com/zybooks/pizzaparty/MainActivity.java
+++ Pizza_Party/app/src/main/java/com/zybooks/pizzaparty/MainActivity.java
@@ -1,12 +1,18 @@
 package com.zybooks.pizzaparty;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText mNumAttendEditText;
     private TextView mNumPizzasTextView;
-    private RadioGroup mHowHungryRadioGroup;
+    private Spinner mHowHungrySpinner;
 
     private final static String TAG = "MainActivity";
 
@@ -28,7 +34,44 @@ public class MainActivity extends AppCompatActivity {
         // Assign the widgets to fields
         mNumAttendEditText = findViewById(R.id.attendEditText);
         mNumPizzasTextView = findViewById(R.id.answerTextView);
-        mHowHungryRadioGroup = findViewById(R.id.hungryRadioGroup);
+        mHowHungrySpinner = findViewById(R.id.hungrySpinner);
+
+        // TextWatcher for attendEditText ie. mNumAttendEditText
+        mNumAttendEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            int i = 0;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mNumPizzasTextView.setText("");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        // Set up spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.hunger_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mHowHungrySpinner.setAdapter(adapter);
+        mHowHungrySpinner.setSelection(0,false);
+        mHowHungrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = (String)parent.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
+                mNumPizzasTextView.setText("");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+
     }
 
     public void calculateClick(View view) {
@@ -43,12 +86,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // Get hunger level selection
-        int checkedId = mHowHungryRadioGroup.getCheckedRadioButtonId();
+        String checkedItem = mHowHungrySpinner.getSelectedItem().toString();
         PizzaCalculator.HungerLevel hungerLevel = PizzaCalculator.HungerLevel.RAVENOUS;
-        if (checkedId == R.id.lightRadioButton) {
+        if (checkedItem.equals("Light")) {
             hungerLevel = PizzaCalculator.HungerLevel.LIGHT;
         }
-        else if (checkedId == R.id.mediumRadioButton) {
+        else if (checkedItem.equals("Medium")) {
             hungerLevel = PizzaCalculator.HungerLevel.MEDIUM;
         }
 
