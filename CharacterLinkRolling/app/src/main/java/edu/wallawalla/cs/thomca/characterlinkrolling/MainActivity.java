2781f14d0820132df17db96aea9b3f997584faa9
thomca <catherine.thomsen@wallawalla.edu>
2021-04-11 12:15:27 -0700

    layout and widgets

--- CharacterLinkRolling/app/src/main/java/edu/wallawalla/cs/thomca/characterlinkrolling/MainActivity.java
+++ CharacterLinkRolling/app/src/main/java/edu/wallawalla/cs/thomca/characterlinkrolling/MainActivity.java
@@ -1,8 +1,16 @@
 package edu.wallawalla.cs.thomca.characterlinkrolling;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,91 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner characterClassSpinner = findViewById(R.id.classSpinner);
+        SwitchCompat shareBetweenClassSwitch = findViewById(R.id.sharingStatusClassSwitch);
+        SwitchCompat saveDiceWCharacterSwitch = findViewById(R.id.saveDiceToCharacterSwitch);
+        SeekBar diceCountSeekBar = findViewById(R.id.diceCountBar);
+        SeekBar diceSidesSeekBar = findViewById(R.id.diceSidesBar);
+
+
+        //setting up spinner for character classes
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.characterClasses, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        characterClassSpinner.setAdapter(adapter);
+        characterClassSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = (String)parent.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        //switch for sharing dice between characters of the same class
+        shareBetweenClassSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // on
+                    int i = 1;
+                } else {
+                    // off
+                    int i = 0;
+                }
+            }
+        });
+        //switch to tag dice to this character
+        saveDiceWCharacterSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // on
+                    int i = 1;
+                } else {
+                    // off
+                    int i = 0;
+                }
+            }
+        });
+        //tracking seek bar for dice count
+        diceCountSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // progress ranges from 0 to max
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int itemInt = seekBar.getProgress();
+                String item = String.valueOf(itemInt);
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //tracking seek bar for dice sides
+        diceSidesSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // progress ranges from 0 to max
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int itemInt = seekBar.getProgress();
+                String item = String.valueOf(itemInt);
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
