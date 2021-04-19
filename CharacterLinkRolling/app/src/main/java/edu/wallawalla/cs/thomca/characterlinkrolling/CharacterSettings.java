dd03712c735b4246e7fc9974332183ce2e23d12c
thomca <catherine.thomsen@wallawalla.edu>
2021-04-18 21:11:27 -0700

    update to intents and pages

--- CharacterLinkRolling/app/src/main/java/edu/wallawalla/cs/thomca/characterlinkrolling/CharacterSettings.java
+++ CharacterLinkRolling/app/src/main/java/edu/wallawalla/cs/thomca/characterlinkrolling/CharacterSettings.java
@@ -9,41 +9,89 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
-import android.widget.SeekBar;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class CharacterSettings extends AppCompatActivity {
 
-    public static final String CHARACTER_NAME = "John Smith";
-
+    public static final String CHARACTER_NAME = "Character Name";
+    public static final String CHARACTER_CLASS = "Character Class";
+    public static final String CHARACTER_SAVE_SETTINGS = "Save Settings";
+    public static boolean saveBasedName;
+    public static boolean saveBasedClass;
+    public static String saveState;
+    public static String characterName;
+    public static int characterClass;
+    private EditText characterNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_settings);
 
-        // Get the character ID from MainActivity
-        // Intent intent = getIntent();
-        // int characterId = intent.getIntExtra(CHARACTER_NAME, R.string.classNull);
-
+        // Get the character info from MainActivity
+        Intent intent = getIntent();
+        characterName = intent.getStringExtra(CHARACTER_NAME);
+        saveState = intent.getStringExtra(CHARACTER_SAVE_SETTINGS);
+        characterClass = intent.getIntExtra(CHARACTER_CLASS, R.string.classNull);
 
         Spinner characterClassSpinner = findViewById(R.id.classSpinner);
         SwitchCompat shareBetweenClassSwitch = findViewById(R.id.sharingStatusClassSwitch);
         SwitchCompat saveDiceWCharacterSwitch = findViewById(R.id.saveDiceToCharacterSwitch);
+        characterNameEditText = findViewById(R.id.characterNameEditText);
+
+        //preset switches
+        if(saveState != null){
+            if(saveState.charAt(0) == 'T'){
+                saveBasedClass = true;
+            }
+            else {
+                saveBasedClass = false;
+            }
+            if(saveState.charAt(1) == 'T'){
+                saveBasedName = true;
+            }
+            else {
+                saveBasedName = false;
+            }
+            shareBetweenClassSwitch.setChecked(saveBasedClass);
+            saveDiceWCharacterSwitch.setChecked(saveBasedName);
+        }
+        // set character name
+        if(characterName != null){
+            characterNameEditText.setText(characterName);
+        }
+
 
         //setting up spinner for character classes
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.characterClasses, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         characterClassSpinner.setAdapter(adapter);
+        //preset spinner
+        if (characterClass != R.string.classNull){
+            characterClassSpinner.setSelection(adapter.getPosition(getResources().getString(characterClass)));
+        }
         characterClassSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String item = (String) parent.getItemAtPosition(position);
                 Toast.makeText(CharacterSettings.this, item, Toast.LENGTH_SHORT).show();
+                // store for pass back
+                if(item.equals("Fighter")){
+                    characterClass = R.string.classFighter;
+                }
+                if(item.equals("Wizard")){
+                    characterClass = R.string.classWizard;
+                }
+                if(item.equals("Cleric")){
+                    characterClass = R.string.classCleric;
+                }
+                if(item.equals("Ranger")){
+                    characterClass = R.string.classRanger;
+                }
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
             }
@@ -54,10 +102,10 @@ public class CharacterSettings extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     // on
-                    int i = 1;
+                    saveBasedClass = true;
                 } else {
                     // off
-                    int i = 0;
+                    saveBasedClass = false;
                 }
             }
         });
@@ -66,13 +114,43 @@ public class CharacterSettings extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     // on
-                    int i = 1;
+                    saveBasedName = true;
                 } else {
                     // off
-                    int i = 0;
+                    saveBasedName = false;
                 }
             }
         });
+
+        //add Text Watcher to store character name
+
+
+    }
+
+    //click finish
+    public void onFinishButtonSelected(android.view.View v){
+        //set up bool return
+        if(saveBasedClass){
+            saveState = "T";
+        }
+        else {
+            saveState = "F";
+        }
+        if(saveBasedName){
+            saveState = saveState + "T";
+        }
+        else{
+            saveState = saveState + "F";
+        }
+
+        characterName = characterNameEditText.getText().toString();
+
+        Intent intent = new Intent();
+        intent.putExtra(CHARACTER_SAVE_SETTINGS,saveState);
+        intent.putExtra(CHARACTER_NAME,characterName);
+        intent.putExtra(CHARACTER_CLASS,characterClass);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 
 }
--- CharacterLinkRolling/app/src/main/java/edu/wallawalla/cs/thomca/characterlinkrolling/MainActivity.java
+++ CharacterLinkRolling/app/src/main/java/edu/wallawalla/cs/thomca/characterlinkrolling/MainActivity.java
@@ -1,32 +1,45 @@
 package edu.wallawalla.cs.thomca.characterlinkrolling;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.SeekBar;
-import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int characterClassId;
+    private int characterClassId = R.string.classNull;
+    private String characterName;
+    private String saveSettings;
     private final int SAVING_SETTINGS = 0;
-
+    private final String KEY_CHARACTER_CLASS = "Character Class";
+    private final String KEY_CHARACTER_NAME = "Character Name";
+    private final String KEY_SAVE_STATE = "Save State";
+    private int diceSides = 10;
+    private int diceCount = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Restore state
+        if (savedInstanceState != null) {
+                characterClassId = savedInstanceState.getInt(KEY_CHARACTER_CLASS);
+                characterName = savedInstanceState.getString(KEY_CHARACTER_NAME);
+                saveSettings = savedInstanceState.getString(KEY_SAVE_STATE);
+        }
+        setClassImage();
+
         SeekBar diceCountSeekBar = findViewById(R.id.diceCountBar);
         SeekBar diceSidesSeekBar = findViewById(R.id.diceSidesBar);
-        characterClassId = R.string.classNull;
 
         //tracking seek bar for dice count
         diceCountSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -41,8 +54,8 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                int itemInt = seekBar.getProgress();
-                String item = String.valueOf(itemInt);
+                diceCount= seekBar.getProgress();
+                String item = String.valueOf(diceCount);
                 Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
             }
         });
@@ -60,21 +73,67 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                int itemInt = seekBar.getProgress();
-                String item = String.valueOf(itemInt);
+                diceSides = seekBar.getProgress();
+                String item = String.valueOf(diceSides);
                 Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    // character settings button
-
-    public void onCharacterSettingsClick(View view) {
-        Intent intent = new Intent(this, CharacterSettings.class);
-        //intent.putExtra(CharacterSettings.CHARACTER_NAME, characterClassId);
-        //startActivityForResult(intent, SAVING_SETTINGS);
-        startActivity(intent);
+    // rolling the dice
+    public void onRollDiceClick(View view){
+        Random randomNumGenerator = new Random();
+        int currentRoll;
+        int finalRoll = 0;
+        for(int i = 0; i < diceCount; i++){
+            currentRoll = randomNumGenerator.nextInt(diceSides) + 1;
+            finalRoll = finalRoll + currentRoll;
+        }
+        String item = String.valueOf(finalRoll);
+        Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
     }
 
+    // character settings button
+    public void onCharacterSettingsClick(View view) {
+        Intent intent = new Intent(this, CharacterSettings.class);
+        intent.putExtra(CharacterSettings.CHARACTER_NAME, characterName);
+        intent.putExtra(CharacterSettings.CHARACTER_CLASS, characterClassId);
+        intent.putExtra(CharacterSettings.CHARACTER_SAVE_SETTINGS, saveSettings);
+        startActivityForResult(intent, SAVING_SETTINGS);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == SAVING_SETTINGS) {
+            characterClassId = data.getIntExtra(CharacterSettings.CHARACTER_CLASS, R.string.classNull);
+            characterName = data.getStringExtra(CharacterSettings.CHARACTER_NAME);
+            saveSettings = data.getStringExtra(CharacterSettings.CHARACTER_SAVE_SETTINGS);
+        }
+        setClassImage();
+    }
+
+    public void setClassImage(){
+        ImageView characterImage = findViewById(R.id.classImage);
+        if (characterClassId == R.string.classCleric){
+            characterImage.setImageResource(R.drawable.clericgraphic);}
+        else if (characterClassId == R.string.classFighter){
+            characterImage.setImageResource(R.drawable.fightergraphic);}
+        else if (characterClassId == R.string.classRanger){
+            characterImage.setImageResource(R.drawable.rangergraphic);}
+        else if (characterClassId == R.string.classWizard){
+            characterImage.setImageResource(R.drawable.wizardgraphic);}
+        else{
+            characterImage.setImageResource(R.drawable.diceicon);
+        }
+    }
+
+    //on save
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_CHARACTER_CLASS,characterClassId);
+        outState.putString(KEY_CHARACTER_NAME,characterName);
+        outState.putString(KEY_SAVE_STATE,saveSettings);
+    }
 }
